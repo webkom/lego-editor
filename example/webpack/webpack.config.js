26d69d3de646ad7b1b2d9db5ff48d2a0af9340d0
Ludvig Hozman <ludvig123@gmail.com>
2019-07-20 17:00:39 +0200

    Add webpack for example build

---
+++ example/webpack/webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = () => {
+  return {
+    output: {
+      path: path.resolve(__dirname, '../build'),
+      filename: '[name]-[hash].js',
+      sourceMapFilename: '[file].map',
+    },
+    entry: path.resolve(__dirname, '../src/index.js'),
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: 'source-map-loader',
+          enforce: 'pre',
+        },
+        {
+          test: /\.js?$/,
+          use: {
+            loader: 'babel-loader',
+          },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.css'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../src/index.html'),
+        filname: './index.html',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+      contentBase: path.resolve(__dirname, '../dist'),
+      hot: true,
+    },
+  };
+};
