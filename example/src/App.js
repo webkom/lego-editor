9cb15bfad80deab8095674538660ab35d51ff01a
Ludvig Hozman <ludvig123@gmail.com>
2021-01-19 23:47:53 +0100

    Reset background to always just be opacity

--- example/src/App.js
+++ example/src/App.js
@@ -3,9 +3,9 @@ import Editor from '../../src/index';
 import '../../src/Editor.css';
 import '../../src/components/Toolbar.css';
 import '../../src/components/ImageUpload.css';
+import '../../src/components/LinkInput.css';
 import 'react-image-crop/dist/ReactCrop.css';
 import './App.css';
-import '../../src/components/LinkInput.css';
 
 const App = () => (
   <div>
