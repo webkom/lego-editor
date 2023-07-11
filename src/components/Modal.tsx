fb390646302e91f76b4162fdaaaa77849deb4840
Ivar Nakken <ivarnakken1441@gmail.com>
2023-07-11 18:40:48 +0200

    Make modal resemble lego-webapp's modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { type ReactNode, useState } from 'react';
 import Modal from 'react-modal';
 import cx from 'classnames';
 
@@ -6,7 +6,7 @@ type Props = {
   onCancel: () => void;
   onSubmit: () => void;
   disabled?: boolean;
-  children?: React.ReactChild[] | React.ReactChild;
+  children?: ReactNode[] | ReactNode;
 };
 
 const LegoModal: React.FC<Props> = ({
@@ -14,38 +14,62 @@ const LegoModal: React.FC<Props> = ({
   onCancel,
   onSubmit,
   disabled,
-}) => (
-  <Modal style={{ overlay: { zIndex: 10 } }} ariaHideApp={false} isOpen={true}>
-    <div className="_legoEditor_modal_wrapper">
-      <div className="_legoEditor_modal_root">
-        {children}
-        <div className="_legoEditor_modal_buttonContainer">
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => {
+    onCancel();
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal
+      style={{ overlay: { zIndex: 10 } }}
+      onRequestClose={closeModal}
+      ariaHideApp={false}
+      isOpen={isOpen}
+    >
+      <div className="_legoEditor_modal_wrapper">
+        <div className="_legoEditor_modal_root">
           <button
+            onClick={closeModal}
             className={cx(
-              '_legoEditor_modal_cancelButton',
+              '_legoEditor_modal_closeButton',
               '_legoEditor_modal_button'
             )}
-            onClick={onCancel}
-            type="button"
           >
-            Avbryt
+            <i className="fa fa-times" />
           </button>
 
-          <button
-            className={cx(
-              '_legoEditor_modal_applyButton',
-              '_legoEditor_modal_button'
-            )}
-            onClick={onSubmit}
-            type="button"
-            disabled={disabled}
-          >
-            Bruk
-          </button>
+          {children}
+          <div className="_legoEditor_modal_buttonContainer">
+            <button
+              className={cx(
+                '_legoEditor_modal_cancelButton',
+                '_legoEditor_modal_button'
+              )}
+              onClick={onCancel}
+              type="button"
+            >
+              Avbryt
+            </button>
+
+            <button
+              className={cx(
+                '_legoEditor_modal_applyButton',
+                '_legoEditor_modal_button'
+              )}
+              onClick={onSubmit}
+              type="button"
+              disabled={disabled}
+            >
+              Bruk
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default LegoModal;
