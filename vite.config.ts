944db702942e04e31f534665910408c54d166fd1
Ivar Nakken <ivarnakken1441@gmail.com>
2023-11-05 22:25:59 +0100

    Implement lego-bricks

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /~(.+)/,
+        replacement: resolve(__dirname, 'node_modules/$1'),
+      },
+    ],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
